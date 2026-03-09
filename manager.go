475fb68fed955305bdd5345ef44898ea2d86eb40
Joshua Matthews <josh.matthews@viam.com>
2026-03-09 16:05:51 -0400

    Fix wg.Go usage

--- manager.go
+++ manager.go
@@ -633,8 +633,6 @@ func (m *Manager) StartBackgroundChecks(ctx context.Context) {
 	}()
 
 	m.activeBackgroundWorkers.Go(func() {
-		defer m.activeBackgroundWorkers.Done()
-
 		ticker := time.NewTicker(osRebootCheckInterval)
 		defer ticker.Stop()
 		for {
